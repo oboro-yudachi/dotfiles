a95a7d0dbc03ca1c05359d51b8bf4637b8464ae3
shoh13 <10_draft_willful@icloud.com>
2025-12-21 23:41:38 +0900

    window decorations

--- wezterm.lua
+++ wezterm.lua
@@ -7,5 +7,6 @@ config.font_size = 12.0
 config.use_ime  = true
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
 
 return config
